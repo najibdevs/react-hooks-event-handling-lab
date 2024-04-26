7d53a309f3ac908dc5fc6683f76040aae6261756
najibdevs <najibsoftwareeng@gmail.com>
2024-04-26 12:41:55 +0300

    react hooks event handling event

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+  const focus = () => {
+    console.log("Good!");
+  };
+
+  const blur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <button onFocus={focus} onBlur={blur}>
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
+
